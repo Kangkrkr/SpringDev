90548a9900f8c5c19aee018043b84307d85ab70f
teamk <kangkrkr@naver.com>
2020-09-26 09:15:02 +0900

    GCP Storage 연동처리

--- src/main/java/com/dev/kangkrkr/web/controller/test/TestController.java
+++ src/main/java/com/dev/kangkrkr/web/controller/test/TestController.java
@@ -1,8 +1,13 @@
 package com.dev.kangkrkr.web.controller.test;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.kangkrkr.repositories.redis.repository.TestRedisRepository;
 import com.dev.kangkrkr.repositories.redis.vo.Test;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
 
 @RequestMapping("/test")
 @RestController
@@ -17,7 +24,12 @@ public class TestController {
 
 	@Autowired
 	private TestRedisRepository testRedisReposytory;
+
+	@Autowired
+	private Storage storage;
 	
+	@Value("${gcp.bucket.name}")
+	private String bucketName;
 	
 	@GetMapping("/save/{id}")
 	public Test saveTest(@PathVariable("id") int id) {
@@ -31,4 +43,19 @@ public class TestController {
 		return saved;
 	}
 	
+	@GetMapping("/storage/{filename}")
+	public ResponseEntity getTextFromFile(@PathVariable("filename") String filename) {
+		Blob blob = storage.get(bucketName, filename);
+		
+		Path downloadPath = Paths.get("download/");
+		File downloadDir  = downloadPath.toFile();
+		
+		if (! downloadDir.exists()) {
+			downloadDir.mkdirs();
+		}
+		
+		blob.downloadTo(downloadPath.resolve(filename));
+		
+		return ResponseEntity.ok(blob.toString());
+	}
 }
